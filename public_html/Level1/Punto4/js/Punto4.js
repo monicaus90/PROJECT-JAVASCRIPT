0f6388331a649610de582f168d5f7c4f24f2d386
MONICA USQUIANO <musquiano@misena.edu.co>
2017-07-23 15:12:25 -0500

    Elaboration point four

--- public_html/Level1/Punto4/js/Punto4.js
+++ public_html/Level1/Punto4/js/Punto4.js
@@ -1,7 +1,12 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var name = prompt ("Enter your full name: ");
+var type = prompt ("Enter type ID: ");
+var id = prompt ("Enter ID: ");
+var gender = prompt ("Enter type gender: ");
+var height = prompt ("Enter your height: ");
+var city = prompt ("Is it true that he lives in Cartago?" + " YES or NO ");
 
-
+console.log ("Hello, my full name is " + name);
+console.log ("My type ID is " + type + " ID " + id);
+console.log ("My biological gender is " + gender);
+console.log ("My height is " + height + " mts");
+console.log ( city + " I live in Cartago");
