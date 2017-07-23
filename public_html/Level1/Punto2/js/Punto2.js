448498d6ac02be902224d0ef9c022fee883414df
MONICA USQUIANO <musquiano@misena.edu.co>
2017-07-23 14:34:50 -0500

    elaboration second point

--- public_html/Level1/Punto2/js/Punto2.js
+++ public_html/Level1/Punto2/js/Punto2.js
@@ -1,7 +1,3 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+var text = ("Hello, this is my first JavaScript script stored in a variable");
+console.log (text);
 
