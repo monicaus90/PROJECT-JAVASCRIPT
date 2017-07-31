145f35470bc118cdb668dbbe8d6bd7840d04da11
MONICA USQUIANO <musquiano@misena.edu.co>
2017-07-31 17:19:00 -0500

    Se resuelve el punto 16

--- public_html/Level1/Punto16/js/Punto16.js
+++ public_html/Level1/Punto16/js/Punto16.js
@@ -1,7 +1,12 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var per1 = prompt("Calculate perimeter of a rectangle. Enter side A: ");
+var per2 = prompt("Calculate perimeter of a rectangle. Enter side B: ");
 
+var area1 = prompt("Calculate area of a rectangle. Enter base: ");
+var area2 = prompt("Calculate area of a rectangle. Enter height: ");
+
+var per= (2*per1)+(2*per2);
+var area = (area1*area2);
+
+console.log ("Total perimeter of a rectangle: " + per + " cms ");
+console.log ("Total area of a rectangle: " + area + " mtrs2");
 
