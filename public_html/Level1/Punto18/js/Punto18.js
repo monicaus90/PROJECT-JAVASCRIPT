2f87cb5d2f3595698b1b182393599b1b3147aab8
MONICA USQUIANO <musquiano@misena.edu.co>
2017-07-31 17:31:36 -0500

    Se resuelve el punto 18

--- public_html/Level1/Punto18/js/Punto18.js
+++ public_html/Level1/Punto18/js/Punto18.js
@@ -1,7 +1,9 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var sal = prompt("Enter worker's salary: ");
+var ret = prompt("Enter percentage (%) of retention at source: ");
 
+var ret2 = (sal*(ret/100));
+var sal2 = sal-ret2;
 
+console.log ("Salary: " + sal);
+console.log ("Retention: " + ret2);
+console.log ("Total to pay: " + sal2);
