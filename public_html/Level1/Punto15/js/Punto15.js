773bfc9f31bc37210086ec82db3a03c5f13161f3
MONICA USQUIANO <musquiano@misena.edu.co>
2017-07-31 16:47:58 -0500

    Se resuelve el punto 15

--- public_html/Level1/Punto15/js/Punto15.js
+++ public_html/Level1/Punto15/js/Punto15.js
@@ -1,7 +1,8 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var age = prompt("Enter your age: ");
 
+if (age>=18) {
+  console.log ("Welcome to party"); 
+} else {
+  console.log ("Sorry, you can not join the party")
+}
 
