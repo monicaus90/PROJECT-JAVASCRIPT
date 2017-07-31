1feb50bb4394207dfc8165584f350dfe69a4917c
MONICA USQUIANO <musquiano@misena.edu.co>
2017-07-31 15:47:34 -0500

    Se resuelve el punto 7

--- public_html/Level1/Punto7/js/Punto7.js
+++ public_html/Level1/Punto7/js/Punto7.js
@@ -1,7 +1,36 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var num1 = Number(prompt("Enter a number: "));
+var num2 = Number(prompt("Enter another number: "));
 
+var exp1 = num1**num2;
+
+var exp2 = num2**num1;
+
+var mult = num1 * num2;
+var res3 = mult ;
+
+var div1 = num1 / num2;
+var res4 = div1;
+
+var div2 = num2 / num1;
+var res5 = div2;
+
+var mod = num1%num2;
+var res6 = mod;
+
+var res = num1 - num2;
+var res7 = res;
+
+var sum = num1 + num2;
+var res8 = sum;
+
+//Print result
+
+console.log ("50 to 10 is: " + exp1);
+console.log ("10 to 50 is: " + exp2);
+console.log ("The multiplication of " +  num1 + " and " + num2 + " is: " + res3);
+console.log ("The division of " + num1 + " and " + num2 + " is: " + res4);
+console.log ("The division of " + num2 + " and " + num1 + " is: " + res5);
+console.log ("The mod of division of " + num1 + " and " + num2 + " is: " + res6);
+console.log ("The substraction of " + num1 + " and " + num2 + " is: " + res7);
+console.log ("The sum of" + num1 + " and " + num2 + " is: " + res8);
 
