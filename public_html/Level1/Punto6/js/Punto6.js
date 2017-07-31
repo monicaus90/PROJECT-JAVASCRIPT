3d32bd732a30a00286f8758719599d68c6be52ba
MONICA USQUIANO <musquiano@misena.edu.co>
2017-07-31 15:45:15 -0500

    Se resuelve el punto 6

--- public_html/Level1/Punto6/js/Punto6.js
+++ public_html/Level1/Punto6/js/Punto6.js
@@ -1,7 +1,41 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var num1 = 50;
+var num2 = 10;
 
+//Base eleveada
+var exp1 = num1**num2;
 
+var exp2 = num2**num1;
+
+//multiplicacion
+var mult = num1 * num2;
+var res3 = mult ;
+
+//division
+var div1 = num1 / num2;
+var res4 = div1;
+
+var div2 = num2 / num1;
+var res5 = div2;
+
+//residuo de la division
+var mod = num1%num2;
+var res6 = mod;
+
+//resta
+var res = num1 - num2;
+var res7 = res;
+
+//suma
+var sum = num1 + num2;
+var res8 = sum;
+
+//Print result
+
+console.log ("50 to 10 is: " + exp1);
+console.log ("10 to 50 is: " + exp2 );
+console.log ("The multiplication of 50 and 10 is: " + res3);
+console.log ("The division of 50 and 10 is: " + res4);
+console.log ("The division of 10 and 50 is: " + res5);
+console.log ("The mod of division of 50 and 10 is: " + res6);
+console.log ("The substraction of 50 and 10 is: " + res7);
+console.log ("The sum of 50 and 10 is: " + res8);
