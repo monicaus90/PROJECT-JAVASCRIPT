9e02cacdab3200fabdedcb4066e0505786adb6ac
MONICA USQUIANO <musquiano@misena.edu.co>
2017-08-01 14:41:31 -0500

    Se resuelve el punto 13

--- public_html/Level1/Punto13/js/Punto13.js
+++ public_html/Level1/Punto13/js/Punto13.js
@@ -1,7 +1,29 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var a = "*";
+var b = "";
+var c = "";
+var d = "    ";
+var e = " ";
 
-
+for (i=1; i<=13; i++) {
+  if (i===1){
+    console.log (b = d + a);
+  }
+    if (i>1 && i<=4) {
+      if (i===3){
+        c = b;
+      }
+      console.log (b = b.slice(1) + a + a);
+    }
+      if (i===5){
+        console.log (c);
+      }
+        if (i===6) {
+          console.log (c);
+        }
+          if (i>=7 && i<=9){
+            console.log (c = c.slice(1) + a + a);
+          }
+            if (i>9) {
+              console.log (c = e + c.slice(0,-2));
+            }
+}
