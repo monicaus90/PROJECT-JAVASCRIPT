591258e8e5b8f50f75ba23f35df49ded3818a20f
MONICA USQUIANO <musquiano@misena.edu.co>
2017-08-01 16:10:51 -0500

    Se resuelve el punto 22

--- public_html/Level1/Punto22/js/Punto22.js
+++ public_html/Level1/Punto22/js/Punto22.js
@@ -1,7 +1,14 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var month = 12*15;
+var day = 12*30*15;
+var hour = 12*30*24*15;
+var mint = 12*30*24*60*15;
+var sec = 12*30*24*60*60*15;
+
+console.log ("Fifteen (15) years are: ");
+console.log ("Months: " + month);
+console.log ("Days: " + day);
+console.log ("Hours: " + hour);
+console.log ("Minutes: " + mint);
+console.log ("Seconds: " + sec);
 
 
