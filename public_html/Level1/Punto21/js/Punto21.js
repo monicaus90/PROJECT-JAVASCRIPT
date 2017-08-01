80b810e8ff3b07aae98bb85772a50a7ab516b99f
MONICA USQUIANO <musquiano@misena.edu.co>
2017-08-01 15:40:51 -0500

    Se resuelve el punto 21

--- public_html/Level1/Punto21/js/Punto21.js
+++ public_html/Level1/Punto21/js/Punto21.js
@@ -1,7 +1,11 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var a = prompt ("Enter side A in cms: ");
+var b = prompt ("Enter side B in cms: ");
+
+var hyp = (a**2) + (b**2);
+var hypt = hyp**0.5;
+
+console.log ("C^2 = a^2 + b^2");
+console.log ("Hypotenuse C: " + hypt);
+
 
 
