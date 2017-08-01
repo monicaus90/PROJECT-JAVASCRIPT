2c9b75c35d10f348e19bbdfc2c3445b6f4ae67de
MONICA USQUIANO <musquiano@misena.edu.co>
2017-08-01 14:22:34 -0500

    Se resuelve el punto 20

--- public_html/Level1/Punto20/js/Punto20.js
+++ public_html/Level1/Punto20/js/Punto20.js
@@ -1,7 +1,23 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var salg = prompt ("Enter salary: ");
+var day =  prompt ("Enter day worked: ");
 
+var salv = 737717;
+var aux = 83140;
 
+var sal1 = (salv *2);
+var sal2 = (salv *4);
+
+var salt = (salg/30)*day;
+
+if (salt<=sal1){
+   var salaux = salt + aux;
+   console.log ("Total salary more bono transport: " + salaux);
+} else if (salt>=sal2) {
+  var ret= prompt ("Enter percentage (%) of retention at source: ");
+    var ret1 = (salt*(ret/100));
+    var ret2 = (salt - ret1);
+    console.log ("Total salary without retention: " + " $" + ret2); 
+} else
+ {
+   console.log ("Total salary without bono transport: " + Math.round(salt));   
+}
