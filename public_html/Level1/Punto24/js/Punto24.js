a7c889391138f3e2e91f84525c60ecffb901e209
MONICA USQUIANO <musquiano@misena.edu.co>
2017-08-03 16:07:32 -0500

    Se resuelve el punto 24

--- public_html/Level1/Punto24/js/Punto24.js
+++ public_html/Level1/Punto24/js/Punto24.js
@@ -1,7 +1,8 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var hour = prompt("Enter # hours: ");
 
+var dayst = hour/24;
+var weekt = hour/(7*24);
 
+console.log ("The hours entered" + hour + " are equivalent to: ");
+console.log ("Total weeks is : " + weekt);
+console.log ("Total dayst is : " + dayst);
