555e644c5f91bf0fad86933fb04b8f92c6ca1146
MONICA USQUIANO <musquiano@misena.edu.co>
2017-08-27 16:24:10 -0500

    Se realiza el punto 37 sin aplicar bootstrap

--- public_html/Level2/Punto37/js/Punto37.js
+++ public_html/Level2/Punto37/js/Punto37.js
@@ -1,7 +1,2 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+$("#text37").text("Hello, this is my number 37 script in JavaScript using Jquery");
 
