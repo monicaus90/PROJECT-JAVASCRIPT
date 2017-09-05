d212535a2332385b89ad794acf969de283d00226
MONICA USQUIANO <musquiano@misena.edu.co>
2017-09-05 15:41:21 -0500

    Se realiza el punto 40 aplicando bootstrap y Jquery

--- public_html/Level2/Punto40/js/Punto40.js
+++ public_html/Level2/Punto40/js/Punto40.js
@@ -1,7 +1,17 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+function salve(){
+  var name = $("#text1").val();
+  var print1 = name;
+  var type = $("#text2").val();
+  var print2 = type;
+  var document = $("#text3").val();
+  var print3 = document;
+  var gender = $("#text4").val();
+  var print4 = gender;
+  var height = $("#text5").val();
+  var print5 = height;
+  var live = $("#text6").val();
+  var print6 = live;
+  var print = ("Hello, my full name is " + name + ", my " + type + " is " + document + ", my gender is " + gender + ", my height is " + height + " mts "+ "and " + live + " I live in Cartago");
+  $("#information").val(print);
+}
+ 
