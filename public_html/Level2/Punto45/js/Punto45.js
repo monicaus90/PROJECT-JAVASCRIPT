cf4fb1b602e3bd384f7138b1d62b1b50bcf42aa3
MONICA USQUIANO <musquiano@misena.edu.co>
2017-09-13 14:39:18 -0500

    Se realiza el punto 45 aplicando bootstrap y Jquery

--- public_html/Level2/Punto45/js/Punto45.js
+++ public_html/Level2/Punto45/js/Punto45.js
@@ -1,7 +1,33 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+$(document).ready(() => {
+  $("#consult").click(() => {
+    var num1 = $("#number1").val();
+    var x = 0, i;
+    for (i = 1; i <= num1; i++) {
+      if (num1 % i === 0) {
+        x++;
+      }
+    }
+    if (x <= 2) {
+      var text1 = "The number entered " + num1 + " is prime";
+      $("#text1").html(text1);
+    } else {
+      var text1 = "The number entered " + num1 + " is not prime ";
+      $("#text1").html(text1);
+    }
+  });
+});
 
+//
+//var num = Number(prompt('Enter a number:'));
+//var x = 0, i; 
+//for (i = 1; i <= num; i++){
+//  if (num%i === 0){
+//    x++;
+//  }
+//}
+//if (x<= 2){
+//  console.log ("The number entered is prime " + num);
+//} else {
+//  console.log ("The number entered is not prime " + num);
+//}
