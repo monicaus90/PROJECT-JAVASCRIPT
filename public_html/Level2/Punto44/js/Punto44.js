a99d195626aab8aac8ba40e95d9b56f37af7d3af
MONICA USQUIANO <musquiano@misena.edu.co>
2017-09-13 12:23:30 -0500

    Se realiza el punto 44 aplicando bootstrap y Jquery

--- public_html/Level2/Punto44/js/Punto44.js
+++ public_html/Level2/Punto44/js/Punto44.js
@@ -1,7 +1,19 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+$(document).ready(()=>{
+  $("#process").click(()=>{
+  var num1 = $("#number1").val();
+  var num2 = $("#number2").val();
+  var sum = "The sum of " + num1 + " and " + num2 + " is: ";
+  var res1 = Number(num1) + Number(num2);
+  var rest = "The subtraction of " + num1 + " and " + num2 + " is: ";
+  var res2 = Number(num1) - Number(num2);
+  var mult = "The multiplication of " + num1 + " and " + num2 + " is: ";
+  var res3 = Number(num1) * Number(num2);
+  var div = "The division of " + num1 + " and " + num2 + " is: ";
+  var res4 = Number(num1) / Number(num2);
+  $("#sum").html(sum + res1);
+  $("#rest").html(rest + res2);
+  $("#mult").html(mult + res3);
+  $("#div").html(div + res4);
+  });
+});
 
